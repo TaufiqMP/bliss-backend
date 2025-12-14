433d32d70bda12db9be38faf72d079af7c5f192c
Taufiq <taufiqmaulanapr@gmail.com>
2025-12-14 22:13:00 +0700

    removed nano id

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const pool = require("../config/db");
-const { nanoid } = require('nanoid');
 const {
   generateAccessToken,
   generateRefreshToken,
