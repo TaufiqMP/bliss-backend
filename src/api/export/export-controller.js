b7a4b2761a304eb86f189c2d0715572708af8002
Nexarion <fendipermadi058@gmail.com>
2025-12-15 07:48:32 +0700

    fix export handler2

--- src/api/export/export-controller.js
+++ src/api/export/export-controller.js
@@ -4,10 +4,9 @@ const userService = require('../../services/user-service');
 exports.sendLeaderboardEmail = async (req, res) => {
     try {
         const { user_id } = req.user;
-        console.log(req.user.email)
-        const { data } = await userService.getUserById(user_id);
-        console.log("user", data.user.email)
-        await emailService.sendEmail(data.user.email);
+        const user = await userService.getUserById(user_id);
+        console.log("user", user)
+        await emailService.sendEmail(user.data.user.email);
 
         res.status(200).json({
             message: "Email leaderboard berhasil dikirim."
